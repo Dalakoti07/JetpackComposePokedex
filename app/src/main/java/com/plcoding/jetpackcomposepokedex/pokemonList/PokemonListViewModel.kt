d52d186333ae4ad588a7c843647189637ce41376
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-09-11 09:53:02 +0530

    added composables for grid item

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemonList/PokemonListViewModel.kt
@@ -0,0 +1,25 @@
+package com.plcoding.jetpackcomposepokedex.pokemonList
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonListViewModel @Inject constructor() : ViewModel(){
+
+    fun calculateDominantColor(drawable: Drawable, onFinish : (Color) -> Unit){
+        val bitmap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bitmap).generate{ palette->
+            palette?.dominantSwatch?.rgb?.let { colorValue ->
+                onFinish(Color(colorValue))
+            }
+        }
+    }
+
+}
