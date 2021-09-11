91e9b5a029e420710eb0274aab04ea2ae44f51fb
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-09-11 09:29:38 +0530

    resolved build issues and added serialised fields

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.plcoding.jetpackcomposepokedex.pokemonList.PokemonListScreen
 import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 import com.plcoding.jetpackcomposepokedex.utils.Constants.ARGS.DOMINANT_COLOR
 import com.plcoding.jetpackcomposepokedex.utils.Constants.ARGS.POKEMON_NAME
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = POKEMON_LIST_SCREEN){
                     composable(POKEMON_LIST_SCREEN){
-
+                        PokemonListScreen(navController = navController)
                     }
                     composable("$POKEMON_DETAIL_SCREEN/${DOMINANT_COLOR}/${POKEMON_NAME}", arguments = listOf(
                         navArgument(DOMINANT_COLOR){
